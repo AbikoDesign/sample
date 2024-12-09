66da0c80c229a201a56c958327a0a5159f1d5a5c
abikok <koichi.abiko.2011@gmail.com>
2024-12-09 22:16:45 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function calculateSum() {
     const number1 = parseFloat(document.getElementById('number1').value);
     const number2 = parseFloat(document.getElementById('number2').value);
-    const sum = number1 + number2;
+    const number3 = parseFloat(document.getElementById('number3').value);
+    const sum = number1 + number2 + number3;
     document.getElementById('result').innerText = sum;
 }
